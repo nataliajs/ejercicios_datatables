16e7e7e156979695c9602fe74ace8983a37e4504
nataliajs <nataliajimenezsanchez@gmail.com>
2015-03-31 12:29:08 +0200

    ejercicios del 1 a 7 realizados

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+"use strict";
+$("#miTabla").DataTable({
+	/*paging: false,
+	filter:false*/
+	"order": [[0,'asc']],
+	"columnDefs":[
+		{
+			"targets":[2],
+			visible:false,
+			"searchable":true
+		},
+		{
+			"targets":[3],
+			visible:false,
+			searchable:false
+		},
+		{
+			"targets":[4],
+			"orderable":false
+		}
+	],
+	"stateSave": true,
+	"language":{
+		url:'scripts/datatables.spanish.lang'
+	}
+});
